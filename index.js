3bb06c93c9a043fd555e901e998fd8453364e6b9
btanmoy <tanmoy@artisanssolutions.com>
2025-02-25 11:39:20 +0530

    function add added in index page

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code 
+
+function add(numbers) {
+    if (!numbers) return 0;
+    
+    let delimiter = /,|\n/;
+    if (numbers.startsWith("//")) {
+        const parts = numbers.split("\n");
+        delimiter = new RegExp(parts[0].slice(2));
+        numbers = parts.slice(1).join("\n");
+    }
+    
+    const numArray = numbers.split(delimiter).map(num => num.trim()).filter(num => num !== "");
+    
+    return numArray.reduce((sum, num) => sum + Number(num), 0);
+}
+
